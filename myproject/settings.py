6eb750249a6d8e8078638f76013fa76a211d6195
Drew Barrett <dfb1998@gmail.com>
2017-04-29 17:07:30 -0400

    added actual template dir

--- myproject/settings.py
+++ myproject/settings.py
@@ -67,7 +67,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
