14fcaacd3d6fbc7418165e45c76ce68e59bca172
Matt <manzi.matthewr@gmail.com>
2017-04-29 13:05:01 -0700

    added Crime model -moob

--- watchdawg/models.py
+++ watchdawg/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Crime(models.Model):
+    is_victim = models.BooleanField(False)
+    crime_type = models.CharField(max_length=400)
+    date = models.DateTimeField('incident date and time')
+    location = models.CharField(max_length=250)
+    elements = models.CharField(max_length=150,validators=[validate_comma_separated_integer_list])
