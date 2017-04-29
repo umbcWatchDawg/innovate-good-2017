85553bb9ff8c622753869f43f31aa0a83080c860
Drew Barrett <dfb1998@gmail.com>
2017-04-29 14:57:31 -0400

    configured postgres maybe?

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
