97a68a645fd643ce424881396c3ba373426fb0cf
Drew Barrett <dfb1998@gmail.com>
2017-04-29 17:14:55 -0400

    fixed url

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('watchdawg.urls')),
+    url(r'^', include('watchdawg.urls')),
     url(r'^soc/', include('social_django.urls', namespace='social')),
 ]
