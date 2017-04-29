267882426e1ed495bce73f0c988f4a44d6def255
Matt <manzi.matthewr@gmail.com>
2017-04-29 14:02:58 -0700

    added back root route, but kept login -moob

--- watchdawg/views.py
+++ watchdawg/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    #return HttpResponse("Hello, world. You've reached watchdawg!")
-    return render(request, 'watchdawg/login.html')
+    return HttpResponse("Hello, world. You've reached watchdawg!")
+
+def login(request):
+    return render(request, '/watchdawg/login.html')
