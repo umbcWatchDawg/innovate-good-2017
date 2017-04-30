9672d579240029f6b826e5f17cf2ef8ee999e485
Matt <manzi.matthewr@gmail.com>
2017-04-29 20:21:25 -0700

    anotherer test for logout

--- watchdawg/urls.py
+++ watchdawg/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import logout
+from django.contrib.auth.views import logout as djlogout
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', logout, {'next-page': '/'}, name='logout'),
+    url(r'^logout/$', djlogout, name='logout'),
     url(r'^report$', login_required(views.report)),
 ]
