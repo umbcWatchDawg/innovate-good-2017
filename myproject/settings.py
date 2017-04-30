dca4c69c13d68c2efea32a5240051540ab4c4e88
Drew Barrett <dfb1998@gmail.com>
2017-04-30 09:37:09 -0400

    reorganize login

--- myproject/settings.py
+++ myproject/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 MIDDLEWARE = [
